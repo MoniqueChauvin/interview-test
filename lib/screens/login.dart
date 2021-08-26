691ce3e69aa7f0cd7ce15b860ce9b6fcb5dea69d
Monique Chauvin <monique-chauvin@hotmail.com>
2021-08-26 01:04:52 -0300

    mudanças no design da tela etc

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,6 +9,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _email = '';
+  String _senha = '';
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,55 +20,84 @@ class _LoginPageState extends State<LoginPage> {
         height: double.infinity,
         child: Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
+          child: SingleChildScrollView(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
                   width: 1000,
                   height: 200,
                   child: Image.network(
-                      'https://icons.iconarchive.com/icons/fazie69/league-of-legends/256/Talon-Dragonblade-icon.png')),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'E-mail', border: OutlineInputBorder()),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: 'Senha', border: OutlineInputBorder()),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
+                    'https://icons.iconarchive.com/icons/fazie69/league-of-legends/256/Talon-Dragonblade-icon.png',
+                  ),
+                ),
+                TextField(
+                  onChanged: (value) {
+                    _email = value;
+                    print(_email);
+                  },
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    _senha = value;
+                    print(_senha);
+                  },
+                  textInputAction: TextInputAction.done,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.pink),
+                      ),
+                      onPressed: () {
+                        if (_email == '' || _senha == '') {
+                          return;
+                        } else {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => TestePage(),
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text('Entrar'),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    ElevatedButton(
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all<Color>(
                             Colors.grey.shade800),
                       ),
                       onPressed: () {},
-                      child: const Text('Cadastrar')),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Colors.grey.shade700),
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => TestePage()));
-                      },
-                      child: const Text('Entrar'))
-                ],
-              )
-            ],
+                      child: const Text('Cadastrar'),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+    ));
   }
 }
